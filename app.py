bce2826e52f0210abac699299f0cc565f336bfce
mic050r <103114387+mic050r@users.noreply.github.com>
2023-07-12 21:55:21 +0900

    나만의 챗봇

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
+import subprocess
+import sys
+
+def update_pip():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+def install_package(package):
+    try:
+        subprocess.check_output([sys.executable, "-m", "pip", "show", package])
+    except subprocess.CalledProcessError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+update_pip()
+required_packages = ["openai", "gradio"]
+for package in required_packages:
+    install_package(package)
+
 import openai
 import gradio
 
-openai.api_key = "xxxxxx"
+openai.api_key = ""
 
-start_sequence = "\nAI:"
+start_sequence = "\nAI: "
 restart_sequence = "\nHuman: "
 
 prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
 
-
 def openai_create(prompt):
     response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
     )
 
     return response.choices[0].text
@@ -28,10 +44,11 @@ def chatgpt_clone(input, history):
     history = history or []
     s = list(sum(history, ()))
     s.append(input)
+    # ["a", "b", "c"]
+    # "a b c"
     inp = ' '.join(s)
     output = openai_create(inp)
     history.append((input, output))
-    print(input, output, history)
     return history, history
 
 
